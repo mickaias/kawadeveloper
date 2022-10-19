7124cb6fd993624f2a6536c51f8e87c7c7f60616
Kawã <106887343+mickaias@users.noreply.github.com>
2022-10-19 13:34:18 -0400

    first commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -23,13 +23,13 @@ const Work = () => {
             {/* Hover */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                  React JS Application
+                  JavaScript Application
               </span>
               <div className='pt-8 text-center '>
                 <a href="">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
-                <a href="">
+                <a href="https://github.com/mickaias/password-generator">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
               </div>
@@ -41,13 +41,13 @@ const Work = () => {
             {/* Hover */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                  React JS Application
+                  HTML, CSS Application
               </span>
               <div className='pt-8 text-center '>
                 <a href="">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
-                <a href="">
+                <a href="https://github.com/mickaias/league-of-legends">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
               </div>
@@ -59,13 +59,13 @@ const Work = () => {
             {/* Hover */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                  React JS Application
+                  HTML, CSS Application
               </span>
               <div className='pt-8 text-center '>
                 <a href="">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
-                <a href="">
+                <a href="https://github.com/mickaias/landind-page">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
               </div>
